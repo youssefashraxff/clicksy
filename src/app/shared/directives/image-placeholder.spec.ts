7a3f6125f6fe99889e1f15116ca44881e2807316
Youssef Ashraf <y.ashraf.ibrahim@gmail.com>
2025-08-26 20:19:29 +0300

    File Structure

---
+++ src/app/shared/directives/image-placeholder.spec.ts
@@ -0,0 +1,8 @@
+import { ImagePlaceholder } from './image-placeholder';
+
+describe('ImagePlaceholder', () => {
+  it('should create an instance', () => {
+    const directive = new ImagePlaceholder();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/shared/directives/image-placeholder.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appImagePlaceholder]'
+})
+export class ImagePlaceholder {
+
+  constructor() { }
+
+}
